6419de7d444a0f640170e21ce966aa5d9b4c0381
kostya05983 <kostya05983@mail.ru>
2020-11-26 22:21:30 +0700

    Solution to first task

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.Linq;
 using System.Linq.Expressions;
 
 namespace ConsoleApp1
@@ -10,52 +11,72 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var interpreator = new Interpretator();
+            interpreator.Interpretate("set a 5\n" +
+                                      "sub a 3\n" +
+                                      "print a\n" +
+                                      "set b 4\n" +
+                                      "print b");
         }
     }
 
     class Interpretator
     {
-        private Stack<IOperation> _stack = Stack();
-        private Dictionary<string, IOperation> operators = new Dictionary<string, IOperation>();
+        private Queue<IOperation> _queue = new Queue<IOperation>();
+        private Dictionary<string, Int32> memory = new Dictionary<string, Int32>();
 
-        void Interpretate(string text)
+        public void Interpretate(string text)
         {
-            String[] lines = text.Split("\n");
+            var lines = text.Split("\n");
 
             foreach (var line in lines)
             {
-                string operatorName = line.Substring(line.IndexOf(" ", StringComparison.Ordinal));
+                var operatorName = line.Substring(0, line.IndexOf(" ", StringComparison.Ordinal));
                 switch (operatorName)
                 {
                     case "set":
                     {
-                        string cutted = line.Substring(line.IndexOf(" "));
-                        operators.Add(new SetOperator(cutted));
+                        var cutted = CutString(line);
+                        _queue.Enqueue(new SetOperator(cutted));
                         break;
                     }
                     case "sub":
                     {
+                        var cutted = CutString(line);
+                        _queue.Enqueue(new SubOperator(cutted));
                         break;
                     }
                     case "rem":
                     {
+                        var cutted = CutString(line);
+                        _queue.Enqueue(new RemOperator(cutted));
                         break;
                     }
                     case "print":
                     {
+                        var cutted = CutString(line);
+                        _queue.Enqueue(new PrintOperator(cutted));
                         break;
                     }
                 }
             }
+
+            while (_queue.Count > 0)
+            {
+                var op = _queue.Dequeue();
+                op.Call(memory);
+            }
+        }
+
+        private string CutString(string line)
+        {
+            return line.Substring(line.IndexOf(" ", StringComparison.Ordinal) + 1);
         }
     }
 
     interface IOperation
     {
-        Dictionary<String, Int32> Dictionary { get; set; }
-
-        void Call();
+        void Call(Dictionary<String, Int32> memory);
     }
 
     class SetOperator : IOperation
@@ -65,15 +86,63 @@ namespace ConsoleApp1
 
         public SetOperator(string init)
         {
-            _name = init.Substring(init.IndexOf(" ", StringComparison.Ordinal));
-            _val = Int32.Parse(init.Substring(init.IndexOf(" ", StringComparison.Ordinal), init.Length));
+            _name = init.Substring(0, init.IndexOf(" ", StringComparison.Ordinal));
+            _val = Int32.Parse(init.Substring(init.IndexOf(" ", StringComparison.Ordinal) + 1));
         }
 
-        public Dictionary<string, int> Dictionary { get; set; }
-
-        public void Call()
+        public void Call(Dictionary<String, Int32> memory)
         {
-            throw new NotImplementedException();
+            memory[_name] = _val;
+        }
+    }
+
+    class SubOperator : IOperation
+    {
+        private string _name;
+        private Int32 _val;
+
+        public SubOperator(string init)
+        {
+            _name = init.Substring(0, init.IndexOf(" ", StringComparison.Ordinal));
+            _val = Int32.Parse(init.Substring(init.IndexOf(" ", StringComparison.Ordinal) + 1));
+        }
+
+        public void Call(Dictionary<String, Int32> memory)
+        {
+            memory[_name] -= _val;
+        }
+    }
+
+    class RemOperator : IOperation
+    {
+        private string _name;
+        private Int32 _val;
+
+        public RemOperator(string init)
+        {
+            _name = init.Substring(0, init.IndexOf(" ", StringComparison.Ordinal));
+            _val = Int32.Parse(init.Substring(init.IndexOf(" ", StringComparison.Ordinal) + 1));
+        }
+
+        public void Call(Dictionary<String, Int32> memory)
+        {
+            memory.Remove(_name);
+        }
+    }
+
+    class PrintOperator : IOperation
+    {
+        private string _name;
+
+        public PrintOperator(string init)
+        {
+            _name = init;
+        }
+
+        public void Call(Dictionary<String, Int32> memory)
+        {
+            var val = memory[_name];
+            Console.WriteLine(val);
         }
     }
 }
