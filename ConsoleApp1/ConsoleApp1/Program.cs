d8c9da569b7c1f730f83367213459ee581919709
kostya05983 <kostya05983@mail.ru>
2020-12-01 00:31:27 +0700

    Add call modfiier

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -12,58 +12,82 @@ namespace ConsoleApp1
         static void Main(string[] args)
         {
             var interpreator = new Interpretator();
-            interpreator.Interpretate("set a 5\n" +
-                                      "sub a 3\n" +
-                                      "print a\n" +
-                                      "set b 4\n" +
-                                      "print b");
+            interpreator.Interpretate("def test\n" +
+                                      "    set a 5\n" +
+                                      "    sub a 3\n" +
+                                      "    print b\n" +
+                                      "set b 7\n" +
+                                      "call test");
         }
     }
 
     class Interpretator
     {
-        private Queue<IOperation> _queue = new Queue<IOperation>();
+        private Dictionary<String, Queue<IOperation>> _program = new Dictionary<string, Queue<IOperation>>();
         private Dictionary<string, Int32> memory = new Dictionary<string, Int32>();
+        private Stack<String> funkStack = new Stack<string>();
 
         public void Interpretate(string text)
         {
+            _program.Add("main", new Queue<IOperation>());
             var lines = text.Split("\n");
 
+            var currentKey = "main";
             foreach (var line in lines)
             {
-                var operatorName = line.Substring(0, line.IndexOf(" ", StringComparison.Ordinal));
+                var trimmed = line.Trim();
+                var operatorName = trimmed.Substring(0, trimmed.IndexOf(" ", StringComparison.Ordinal));
+                if (!line.StartsWith("    ") && funkStack.Count > 0)
+                {
+                    currentKey = funkStack.Pop();
+                }
+
                 switch (operatorName)
                 {
+                    case "def":
+                        var name = CutString(trimmed);
+                        funkStack.Push(currentKey);
+                        currentKey = name;
+
+                        _program.Add(name, new Queue<IOperation>());
+                        break;
                     case "set":
                     {
-                        var cutted = CutString(line);
-                        _queue.Enqueue(new SetOperator(cutted));
+                        var cutted = CutString(trimmed);
+                        _program[currentKey].Enqueue(new SetOperator(cutted));
                         break;
                     }
                     case "sub":
                     {
-                        var cutted = CutString(line);
-                        _queue.Enqueue(new SubOperator(cutted));
+                        var cutted = CutString(trimmed);
+                        _program[currentKey].Enqueue(new SubOperator(cutted));
                         break;
                     }
                     case "rem":
                     {
-                        var cutted = CutString(line);
-                        _queue.Enqueue(new RemOperator(cutted));
+                        var cutted = CutString(trimmed);
+                        _program[currentKey].Enqueue(new RemOperator(cutted));
                         break;
                     }
                     case "print":
                     {
-                        var cutted = CutString(line);
-                        _queue.Enqueue(new PrintOperator(cutted));
+                        var cutted = CutString(trimmed);
+                        _program[currentKey].Enqueue(new PrintOperator(cutted));
+                        break;
+                    }
+                    case "call":
+                    {
+                        var cutted = CutString(trimmed);
+                        _program[currentKey].Enqueue(new CallOperator(cutted, _program));
                         break;
                     }
                 }
             }
 
-            while (_queue.Count > 0)
+            var queue = _program["main"];
+            while (queue.Count > 0)
             {
-                var op = _queue.Dequeue();
+                var op = queue.Dequeue();
                 op.Call(memory);
             }
         }
@@ -127,6 +151,28 @@ namespace ConsoleApp1
         public void Call(Dictionary<String, Int32> memory)
         {
             memory.Remove(_name);
+        }
+    }
+
+    class CallOperator : IOperation
+    {
+        private string _name;
+        private Dictionary<String, Queue<IOperation>> _dictionary;
+
+        public CallOperator(string init, Dictionary<String, Queue<IOperation>> dictionary)
+        {
+            this._name = init;
+            this._dictionary = dictionary;
+        }
+
+        public void Call(Dictionary<string, int> memory)
+        {
+            var queue = _dictionary[_name];
+            while (queue.Count > 0)
+            {
+                var op = queue.Dequeue();
+                op.Call(memory);
+            }
         }
     }
 
