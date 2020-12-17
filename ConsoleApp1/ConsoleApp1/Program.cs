cbe840b21f8d7a7255a66ca67cc44b1ba700ac93
kostya05983 <kostya05983@mail.ru>
2020-12-17 22:57:37 +0700

    Make better solution

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -51,34 +51,39 @@ namespace ConsoleApp1
 
         public int CalledLine { get; }
 
-        public Dictionary<int, IOperation> Operations { get; }
+        public LinkedList<IOperation> Operations { get; }
 
-        public int CurrentRuntimeLine { get; set; }
+        public LinkedListNode<IOperation> CurrentRuntimeOperator { get; set; }
 
-        public Frame(string name, Dictionary<int, IOperation> operations, int calledLine)
+        public int CurrentLine
+        {
+            get => CurrentRuntimeOperator.Value.ParsedLine;
+        }
+
+        public Frame(string name, LinkedList<IOperation> operations, int calledLine)
         {
             Name = name;
             Operations = operations;
             CalledLine = calledLine;
-            CurrentRuntimeLine = Operations.Count != 0 ? Operations.Keys.Min() : 0;
+            CurrentRuntimeOperator = operations.First;
         }
 
         public bool ExecuteFrameLine(Dictionary<string, Variable> memory)
         {
-            var op = Operations[CurrentRuntimeLine];
-            op.Call(memory, CurrentRuntimeLine);
-            CurrentRuntimeLine++;
+            var op = CurrentRuntimeOperator.Value;
+            op.Call(memory);
+            CurrentRuntimeOperator = CurrentRuntimeOperator.Next;
             return op is CallOperator;
         }
 
         public bool isOver()
         {
-            return !Operations.ContainsKey(CurrentRuntimeLine);
+            return CurrentRuntimeOperator == null;
         }
 
         public void Reset()
         {
-            CurrentRuntimeLine = Operations.Count != 0 ? Operations.Keys.Min() : 0;
+            CurrentRuntimeOperator = Operations.First;
         }
     }
 
@@ -90,9 +95,13 @@ namespace ConsoleApp1
         private List<int> breakPointLines = new List<int>();
         private int? lastBreakLine;
 
+        private const int NEW_CALL = 1;
+        private const int INTERRUPT = -1;
+        private const int OVER = 0;
+
         public Interpretator()
         {
-            program.Add("main", new Frame("main", new Dictionary<int, IOperation>(), 0));
+            program.Add("main", new Frame("main", new LinkedList<IOperation>(), 0));
         }
 
         public void Interpretate(string text)
@@ -134,8 +143,17 @@ namespace ConsoleApp1
                     if (frame.ExecuteFrameLine(memory))
                     {
                         var newFrame = runtime.Peek();
-                        RunFrame(newFrame, true);
-                        runtime.Pop();
+                        var i = 1;
+                        while (RunFrame(newFrame, true) == NEW_CALL)
+                        {
+                            i++;
+                        }
+
+                        while (i > 0)
+                        {
+                            runtime.Pop();
+                            i--;
+                        }
                     }
 
                     break;
@@ -171,31 +189,32 @@ namespace ConsoleApp1
                         funkStack.Push(currentFrame);
                         currentFrame = cutted;
 
-                        program.Add(cutted, new Frame(cutted, new Dictionary<int, IOperation>(), currentLine));
+                        program.Add(cutted, new Frame(cutted, new LinkedList<IOperation>(), currentLine));
                         break;
                     case "set":
                     {
-                        program[currentFrame].Operations[currentLine] = new SetOperator(cutted);
+                        program[currentFrame].Operations.AddLast(new SetOperator(cutted, currentLine));
                         break;
                     }
                     case "sub":
                     {
-                        program[currentFrame].Operations[currentLine] = new SubOperator(cutted);
+                        program[currentFrame].Operations.AddLast(new SubOperator(cutted, currentLine));
                         break;
                     }
                     case "rem":
                     {
-                        program[currentFrame].Operations[currentLine] = new RemOperator(cutted);
+                        program[currentFrame].Operations.AddLast(new RemOperator(cutted, currentLine));
                         break;
                     }
                     case "print":
                     {
-                        program[currentFrame].Operations[currentLine] = new PrintOperator(cutted);
+                        program[currentFrame].Operations.AddLast(new PrintOperator(cutted, currentLine));
                         break;
                     }
                     case "call":
                     {
-                        program[currentFrame].Operations[currentLine] = new CallOperator(cutted, program, runtime);
+                        program[currentFrame].Operations
+                            .AddLast(new CallOperator(cutted, currentLine, program, runtime));
                         break;
                     }
                     case "add":
@@ -241,10 +260,10 @@ namespace ConsoleApp1
                 var result = RunFrame(frame, false);
                 switch (result)
                 {
-                    case 0:
+                    case OVER:
                         runtime.Pop();
                         break;
-                    case -1:
+                    case INTERRUPT:
                         return;
                 }
             }
@@ -254,21 +273,21 @@ namespace ConsoleApp1
         {
             while (!frame.isOver())
             {
-                if (breakPointLines.Contains(frame.CurrentRuntimeLine) && !isIgnoreBreak &&
-                    frame.CurrentRuntimeLine != lastBreakLine)
+                if (breakPointLines.Contains(frame.CurrentLine) && !isIgnoreBreak &&
+                    frame.CurrentLine != lastBreakLine)
                 {
-                    lastBreakLine = frame.CurrentRuntimeLine;
-                    return -1;
+                    lastBreakLine = frame.CurrentLine;
+                    return INTERRUPT;
                 }
 
                 lastBreakLine = null;
                 if (frame.ExecuteFrameLine(memory))
                 {
-                    return 1;
+                    return NEW_CALL;
                 }
             }
 
-            return 0;
+            return OVER;
         }
 
         private string CutString(string line)
@@ -277,9 +296,11 @@ namespace ConsoleApp1
         }
     }
 
-    interface IOperation
+    abstract class IOperation
     {
-        void Call(Dictionary<string, Variable> memory, int currentLine);
+        public abstract int ParsedLine { get; }
+
+        public abstract void Call(Dictionary<string, Variable> memory);
     }
 
     class SetOperator : IOperation
@@ -287,15 +308,18 @@ namespace ConsoleApp1
         private string name;
         private Int32 val;
 
-        public SetOperator(string init)
+        public override int ParsedLine { get; }
+
+        public SetOperator(string init, int parsedLine)
         {
             name = init.Substring(0, init.IndexOf(" ", StringComparison.Ordinal));
             val = Int32.Parse(init.Substring(init.IndexOf(" ", StringComparison.Ordinal) + 1));
+            this.ParsedLine = parsedLine;
         }
 
-        public void Call(Dictionary<string, Variable> memory, int currentLine)
+        public override void Call(Dictionary<string, Variable> memory)
         {
-            memory[name] = new Variable(val, currentLine);
+            memory[name] = new Variable(val, ParsedLine);
         }
     }
 
@@ -303,31 +327,35 @@ namespace ConsoleApp1
     {
         private string name;
         private Int32 val;
+        public override int ParsedLine { get; }
 
-        public SubOperator(string init)
+        public SubOperator(string init, int parsedLine)
         {
             name = init.Substring(0, init.IndexOf(" ", StringComparison.Ordinal));
             val = Int32.Parse(init.Substring(init.IndexOf(" ", StringComparison.Ordinal) + 1));
+            this.ParsedLine = parsedLine;
         }
 
 
-        public void Call(Dictionary<String, Variable> memory, int currentLine)
+        public override void Call(Dictionary<string, Variable> memory)
         {
             memory[name] = new Variable(
-                memory[name].Value - val, currentLine);
+                memory[name].Value - val, ParsedLine);
         }
     }
 
     class RemOperator : IOperation
     {
         private string name;
+        public override int ParsedLine { get; }
 
-        public RemOperator(string init)
+        public RemOperator(string init, int parsedLine)
         {
             name = init.Substring(0, init.IndexOf(" ", StringComparison.Ordinal));
+            this.ParsedLine = parsedLine;
         }
 
-        public void Call(Dictionary<String, Variable> memory, int currentLine)
+        public override void Call(Dictionary<string, Variable> memory)
         {
             memory.Remove(name);
         }
@@ -338,9 +366,11 @@ namespace ConsoleApp1
         private string name;
         private Dictionary<String, Frame> program;
         private Stack<Frame> runtime;
+        public override int ParsedLine { get; }
 
         public CallOperator(
             string init,
+            int parsedLine,
             Dictionary<String, Frame> program,
             Stack<Frame> _runtime
         )
@@ -348,25 +378,28 @@ namespace ConsoleApp1
             this.name = init;
             this.program = program;
             this.runtime = _runtime;
+            this.ParsedLine = parsedLine;
         }
 
-        public void Call(Dictionary<string, Variable> memory, int currentLine)
+        public override void Call(Dictionary<string, Variable> memory)
         {
             var frame = program[name];
-            runtime.Push(new Frame(frame.Name, frame.Operations, currentLine));
+            runtime.Push(new Frame(frame.Name, frame.Operations, ParsedLine));
         }
     }
 
     class PrintOperator : IOperation
     {
         private string name;
+        public override int ParsedLine { get; }
 
-        public PrintOperator(string init)
+        public PrintOperator(string init, int parsedLine)
         {
             name = init;
+            this.ParsedLine = parsedLine;
         }
 
-        public void Call(Dictionary<String, Variable> memory, int currentLine)
+        public override void Call(Dictionary<string, Variable> memory)
         {
             var val = memory[name];
             Console.WriteLine(val.Value);
